73314d629ce3c082fac5265cffb2022384de5a28
marcin1997garbarczyk <marcin1997garbarczyk@gmail.com>
2020-08-02 18:24:33 +0200

    first-commit

---
+++ KoncowkiZBudowy/KoncowkiZBudowy/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KoncowkiZBudowy
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        protected override void OnStart()
+        {
+            Device.OpenUri(new Uri("https://koncowkizbudowy.pl/"));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+            Device.OpenUri(new Uri("https://koncowkizbudowy.pl/"));
+        }
+    }
+
+}
